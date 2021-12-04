d5695f984c4341bf28369952b50947ac09f88c3a
Zul Asyraf <46576588+bukanzul@users.noreply.github.com>
2021-12-05 06:13:54 +0800

    v1.0

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -10,10 +10,36 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-
-      ),
+      backgroundColor: Color(0xFF18224A),
+        body: Container(
+        height: size.height,
+        width: double.infinity,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned(
+              top: -23,
+              left: -10,
+              child: Image.asset('assets/right.png',
+              width: size.width * 0.4)
+            ),
+            Positioned(
+                bottom: 10,
+                left: 0,
+                child: Image.asset('assets/bottomLeft.png',
+                    width: size.width * 0.2)
+            ),
+            // Positioned(
+            //     bottom: 0,
+            //     right: 0,
+            //     child: Image.asset('assets/bottomRight.png',
+            //         width: size.width * 0.2)
+            // ),
+          ],
+        )
+      )
     );
   }
 }
